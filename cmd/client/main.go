5069d57320c17967823d321813ba351039542d8a
Sara Dickinson <sara@sinodun.com>
2021-08-16 13:55:45 +0100

    Update code to match -03 draft version (#8)
    Co-authored-by: Jan Vcelak <jvcelak@ns1.com>

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"crypto/tls"
+	"encoding/binary"
+	"errors"
 	"flag"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"os"
 	"sync"
+	"time"
 
 	"github.com/lucas-clemente/quic-go"
 	"github.com/miekg/dns"
@@ -18,10 +21,15 @@ type Query struct {
 }
 
 func main() {
+	os.Exit(main2())
+}
+
+func main2() int {
 	var (
 		server    string
 		dnssec    bool
 		recursion bool
+		keysPath  string
 		queries   []Query
 	)
 
@@ -30,14 +38,15 @@ func main() {
 		flag.PrintDefaults()
 	}
 
-	flag.StringVar(&server, "server", "127.0.0.1:8853", "DNS-over-QUIC server to use.")
+	flag.StringVar(&server, "server", "127.0.0.1:784", "DNS-over-QUIC server to use.")
 	flag.BoolVar(&dnssec, "dnssec", true, "Send DNSSEC OK flag.")
 	flag.BoolVar(&recursion, "recursion", true, "Send RD flag.")
+	flag.StringVar(&keysPath, "export_keys_path", "", "File name to export session keys for decryption.")
 	flag.Parse()
 
 	if flag.NArg() == 0 || flag.NArg()%2 != 0 {
 		flag.Usage()
-		os.Exit(1)
+		return 1
 	}
 
 	for i := 0; (i + 1) < flag.NArg(); i += 2 {
@@ -45,20 +54,36 @@ func main() {
 		qtype, ok := dns.StringToType[flag.Arg(i+1)]
 		if !ok {
 			fmt.Fprintf(os.Stderr, "invalid qtype: %s\n", flag.Arg(i+1))
-			os.Exit(1)
+			return 1
 		}
+		if qtype == dns.TypeIXFR {
+			// TODO: Allow user to pass in serial number for IXFR
+			fmt.Fprintf(os.Stderr, "skipping unsupported qtype: %s\n", flag.Arg(i+1))
+		} else {
+			queries = append(queries, Query{qname, qtype})
+		}
+	}
 
-		queries = append(queries, Query{qname, qtype})
+	var keyLog io.Writer
+	if keysPath != "" {
+		w, err := os.OpenFile(keysPath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "failed to open file for session keys: %s\n", err)
+			return 1
+		}
+		defer w.Close()
+		keyLog = w
 	}
 
 	tls := tls.Config{
 		InsecureSkipVerify: true,
-		NextProtos:         []string{"doq-i02"},
+		NextProtos:         []string{"doq-i03"},
+		KeyLogWriter:       keyLog,
 	}
 	session, err := quic.DialAddr(server, &tls, nil)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "failed to connect to the server: %s\n", err)
-		os.Exit(1)
+		return 1
 	}
 	defer session.CloseWithError(0, "") // TODO: Is this how the session should be closed?
 
@@ -69,11 +94,9 @@ func main() {
 
 	for _, query := range queries {
 		go func(query Query) {
-			resp, err := SendQuery(session, &query, dnssec, recursion)
+			err := SendQuery(session, &query, dnssec, recursion, print)
 			if err != nil {
 				print <- fmt.Sprintf("failed to send query: %s\n", err)
-			} else {
-				print <- resp.String()
 			}
 			wg.Done()
 		}(query)
@@ -87,39 +110,64 @@ func main() {
 	for p := range print {
 		fmt.Println(p)
 	}
+
+	return 0
 }
 
-func SendQuery(session quic.Session, query *Query, dnssec, recursion bool) (*dns.Msg, error) {
+func SendQuery(session quic.Session, query *Query, dnssec, recursion bool, print chan (string)) error {
 	stream, err := session.OpenStream()
 	if err != nil {
-		return nil, fmt.Errorf("open stream: %w", err)
+		return fmt.Errorf("open stream: %w", err)
 	}
 
 	msg := dns.Msg{}
 	msg.SetQuestion(query.Name, query.Type)
 	msg.RecursionDesired = recursion
 	msg.SetEdns0(4096, dnssec)
+	msg.Id = 0
 	wire, err := msg.Pack()
 	if err != nil {
 		stream.Close()
-		return nil, fmt.Errorf("pack query: %w", err)
+		return fmt.Errorf("pack query: %w", err)
 	}
 
-	_, err = stream.Write(wire)
+	bundle := make([]byte, 2+len(wire))
+	binary.BigEndian.PutUint16(bundle, uint16(len(wire)))
+	copy(bundle[2:], wire)
+	_, err = stream.Write(bundle)
 	stream.Close()
 	if err != nil {
-		return nil, fmt.Errorf("send query: %w", err)
+		return fmt.Errorf("send query: %w", err)
 	}
 
-	rwire, err := ioutil.ReadAll(stream)
-	if err != nil {
-		return nil, fmt.Errorf("receive response: %w", err)
-	}
+	stream.SetDeadline(time.Now().Add(1 * time.Second))
 
-	err = msg.Unpack(rwire)
-	if err != nil {
-		return nil, fmt.Errorf("decode response: %w", err)
-	}
+	for {
+		var length uint16
+		if err := binary.Read(stream, binary.BigEndian, &length); err != nil {
+			// Ignore timeout related errors as this is how we close this connection for now
+			if errors.Is(err, os.ErrDeadlineExceeded) {
+				return nil
+			}
+			return fmt.Errorf("read response length: %w", err)
+		}
 
-	return &msg, nil
+		buf := make([]byte, length)
+		_, err := io.ReadFull(stream, buf)
+		if err != nil {
+			return fmt.Errorf("read response payload: %w", err)
+		}
+
+		resp := dns.Msg{}
+		err = resp.Unpack(buf)
+		if err != nil {
+			return fmt.Errorf("decode response: %w", err)
+		}
+		print <- resp.String()
+		switch msg.Question[0].Qtype {
+		case dns.TypeAXFR, dns.TypeIXFR:
+		default:
+			return nil
+		}
+	}
 }
--- cmd/proxy/main.go
+++ cmd/proxy/main.go
@@ -2,18 +2,23 @@ package main
 
 import (
 	"context"
+	"crypto/rand"
 	"crypto/tls"
+	"encoding/binary"
+	"errors"
 	"flag"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net"
 	"os"
 	"os/signal"
 	"sync"
 	"syscall"
+	"time"
 
 	"github.com/go-kit/kit/log"
 	quic "github.com/lucas-clemente/quic-go"
+	"github.com/miekg/dns"
 	"github.com/oklog/run"
 )
 
@@ -59,16 +64,16 @@ func main() {
 
 func loop(l log.Logger, ctx context.Context) error {
 	var (
-		addr       string
-		tlsCert    string
-		tlsKey     string
-		udpBackend string
+		addr    string
+		tlsCert string
+		tlsKey  string
+		backend string
 	)
 
-	flag.StringVar(&addr, "listen", "127.0.0.1:8853", "UDP address to listen on.")
+	flag.StringVar(&addr, "listen", "127.0.0.1:784", "UDP address to listen on.")
 	flag.StringVar(&tlsCert, "cert", "cert.pem", "TLS certificate path.")
 	flag.StringVar(&tlsKey, "key", "key.pem", "TLS key path.")
-	flag.StringVar(&udpBackend, "udp_backend", "8.8.4.4:53", "UDP of backend server.")
+	flag.StringVar(&backend, "backend", "8.8.4.4:53", "IP of backend server.")
 
 	flag.Parse()
 
@@ -79,7 +84,7 @@ func loop(l log.Logger, ctx context.Context) error {
 
 	tls := tls.Config{
 		Certificates: []tls.Certificate{cert},
-		NextProtos:   []string{"doq-i02"},
+		NextProtos:   []string{"doq-i03"},
 	}
 
 	listener, err := quic.ListenAddr(addr, &tls, nil)
@@ -102,19 +107,19 @@ func loop(l log.Logger, ctx context.Context) error {
 		l := log.With(l, "client", session.RemoteAddr())
 		wg.Add(1)
 		go func() {
-			handleClient(l, ctx, session, udpBackend)
+			handleClient(l, ctx, session, backend)
 			wg.Done()
 		}()
 	}
 
 }
 
-func handleClient(l log.Logger, ctx context.Context, session quic.Session, udpBackend string) {
+func handleClient(l log.Logger, ctx context.Context, session quic.Session, backend string) {
 	l.Log("msg", "session accepted")
 
 	var (
 		err error
-		wg sync.WaitGroup = sync.WaitGroup{}
+		wg  sync.WaitGroup = sync.WaitGroup{}
 	)
 
 	defer func() {
@@ -143,7 +148,7 @@ func handleClient(l log.Logger, ctx context.Context, session quic.Session, udpBa
 				l.Log("msg", "stream closed")
 			}()
 
-			if err := handleStream(stream, udpBackend); err != nil {
+			if err := handleStream(stream, backend); err != nil {
 				l.Log("msg", "stream failure", "err", err)
 			}
 		}()
@@ -152,34 +157,110 @@ func handleClient(l log.Logger, ctx context.Context, session quic.Session, udpBa
 	wg.Wait()
 }
 
-func handleStream(stream quic.Stream, udpBackend string) error {
+func handleStream(stream quic.Stream, backend string) error {
 	defer stream.Close()
 
-	data, err := ioutil.ReadAll(stream)
+	wireLength := make([]byte, 2)
+	_, err := io.ReadFull(stream, wireLength)
 	if err != nil {
-		return fmt.Errorf("read query: %w", err)
+		return fmt.Errorf("read query length: %w", err)
 	}
 
-	conn, err := net.Dial("udp", udpBackend)
+	length := binary.BigEndian.Uint16(wireLength)
+
+	wireQuery := make([]byte, length)
+	_, err = io.ReadFull(stream, wireQuery)
 	if err != nil {
-		return fmt.Errorf("connect to backend: %w", err)
+		return fmt.Errorf("read query payload: %w", err)
 	}
 
-	_, err = conn.Write(data)
+	query := dns.Msg{}
+	err = query.Unpack(wireQuery)
 	if err != nil {
-		return fmt.Errorf("send query to backend: %w", err)
+		return fmt.Errorf("could not decode query: %w", err)
 	}
 
-	buf := make([]byte, 4096)
-	size, err := conn.Read(buf)
+	var id uint16
+	err = binary.Read(rand.Reader, binary.BigEndian, &id)
 	if err != nil {
-		return fmt.Errorf("read response from backend: %w", err)
+		return fmt.Errorf("generating random id failed: %w", err)
 	}
-	buf = buf[:size]
 
-	_, err = stream.Write(buf)
-	if err != nil {
-		return fmt.Errorf("send response: %w", err)
+	if len(query.Question) != 0 && (query.Question[0].Qtype == dns.TypeAXFR || query.Question[0].Qtype == dns.TypeIXFR) {
+		timeout := 3 * time.Second
+		conn, err := net.DialTimeout("tcp", backend, timeout)
+
+		if err != nil {
+			return fmt.Errorf("connect to TCP backend: %w", err)
+		}
+		defer conn.Close()
+
+		bundle := make([]byte, 0)
+		bundle = append(bundle, wireLength...)
+		bundle = append(bundle, wireQuery...)
+
+		binary.BigEndian.PutUint16(bundle[2:], uint16(id))
+		_, err = conn.Write(bundle)
+		if err != nil {
+			return fmt.Errorf("send query to TCP backend: %w", err)
+		}
+
+		conn.SetReadDeadline(time.Now().Add(timeout))
+
+		for {
+			var length uint16
+			if err := binary.Read(conn, binary.BigEndian, &length); err != nil {
+				// Ignore timeout related errors as that is how we close the connection for now
+				if errors.Is(err, os.ErrDeadlineExceeded) {
+					return nil
+				}
+				return fmt.Errorf("read length from TCP backend: %w", err)
+			}
+
+			buf := make([]byte, length)
+			_, err := io.ReadFull(conn, buf)
+			if err != nil {
+				return fmt.Errorf("read response from TCP backend: %w", err)
+			}
+
+			bundle := make([]byte, 2+length)
+			binary.BigEndian.PutUint16(bundle, uint16(length))
+			copy(bundle[2:], buf)
+			binary.BigEndian.PutUint16(bundle[2:], uint16(0))
+			_, err = stream.Write(bundle)
+			if err != nil {
+				return fmt.Errorf("send response: %w", err)
+			}
+		}
+
+	} else {
+		conn, err := net.Dial("udp", backend)
+		if err != nil {
+			return fmt.Errorf("connect to UDP backend: %w", err)
+		}
+
+		binary.BigEndian.PutUint16(wireQuery, uint16(id))
+		_, err = conn.Write(wireQuery)
+		if err != nil {
+			return fmt.Errorf("send query to UDP backend: %w", err)
+		}
+
+		buf := make([]byte, 4096)
+		size, err := conn.Read(buf)
+		if err != nil {
+			return fmt.Errorf("read response from UDP backend: %w", err)
+		}
+		buf = buf[:size]
+
+		bundle := make([]byte, 2+len(buf))
+		binary.BigEndian.PutUint16(bundle, uint16(len(buf)))
+		copy(bundle[2:], buf)
+		binary.BigEndian.PutUint16(bundle[2:], uint16(0))
+
+		_, err = stream.Write(bundle)
+		if err != nil {
+			return fmt.Errorf("send response: %w", err)
+		}
 	}
 	return nil
 }
